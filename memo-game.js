686f4571ed7ae8b7ef927de593268041899c61cd
Piotr Maksam <pmaksam@gmail.com>
2022-09-04 23:16:24 +0200

    some changes

---
+++ memo-game.js
@@ -0,0 +1,18 @@
+const cardArray = [
+  {
+    name: 'beer',
+    image: 'images/beer icon.svg'
+  },
+  {
+    name: 'beer',
+    image: 'images/beer icon.svg'
+  }
+];
+
+cardArray.sort(()=> 0.5 - Math.random());
+
+const gridDisplay = document.querySelector('#grid');
+
+function createGrid(){
+  
+}
