36803c268add446c5d7bf5058d39b65f5e6d6e21
Piotr Maksam <pmaksam@gmail.com>
2022-09-05 10:01:40 +0200

    added some functionality

--- memo-game.js
+++ memo-game.js
@@ -4,8 +4,64 @@ const cardArray = [
     image: 'images/beer icon.svg'
   },
   {
+    name: 'cheeseburger',
+    image: 'images/cheeseburger.png'
+  },
+  {
+    name: 'fries',
+    image: 'images/fries.png'
+  },
+  {
+    name: 'hotdog',
+    image: 'images/hotdog.png'
+  },
+  {
+    name: 'ice-cream',
+    image: 'images/ice-cream.png'
+  },
+  {
+    name: 'kapitan bomba',
+    image: 'images/kapitan bomba.png'
+  },
+  {
+    name: 'milkshake',
+    image: 'images/milkshake.png'
+  },
+  {
+    name: 'pizza',
+    image: 'images/pizza.png'
+  },
+  {
     name: 'beer',
     image: 'images/beer icon.svg'
+  },
+  {
+    name: 'cheeseburger',
+    image: 'images/cheeseburger.png'
+  },
+  {
+    name: 'fries',
+    image: 'images/fries.png'
+  },
+  {
+    name: 'hotdog',
+    image: 'images/hotdog.png'
+  },
+  {
+    name: 'ice-cream',
+    image: 'images/ice-cream.png'
+  },
+  {
+    name: 'kapitan bomba',
+    image: 'images/kapitan bomba.png'
+  },
+  {
+    name: 'milkshake',
+    image: 'images/milkshake.png'
+  },
+  {
+    name: 'pizza',
+    image: 'images/pizza.png'
   }
 ];
 
@@ -13,6 +69,24 @@ cardArray.sort(()=> 0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid');
 
+const cardsChosen = [];
+
 function createGrid(){
-  
+  for (let i = 0; i<cardArray.length;i++){
+    const card = document.createElement('img');
+    card.setAttribute('src','images/blank.png');
+    card.setAttribute('data-id', i);
+    card.addEventListener('click', flipCard);
+    gridDisplay.appendChild(card);
+  }
+}
+
+createGrid();
+
+function flipCard(){
+  let cardId = this.getAttribute('data-id');
+  cardsChosen.push(cardArray[cardId].name);
+  console.log(cardArray[cardId]);
+  console.log(cardsChosen);
+  this.setAttribute('src', cardArray[cardId].image);
 }
